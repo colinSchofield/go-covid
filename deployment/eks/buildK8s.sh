8c210fe43af45cd77bb95186cb0e523e5dbb71cb
Colin Schofield <colin.schofield@comparethemarket.com.au>
2022-09-13 15:22:24 +1000

    eks and K8s -- AWS via ServiceAccount

--- deployment/eks/buildK8s.sh
+++ deployment/eks/buildK8s.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+# Create the EKS cluster
 eksctl create cluster --config-file go-web.yaml
+# The OIDC Connect
+eksctl utils associate-iam-oidc-provider --cluster go-web --approve
+
+# The ServiceAccount and Role given the policy
+eksctl create iamserviceaccount \
+    --name go-web-service-account \
+    --namespace default \
+    --cluster go-web \
+    --role-name "go-web-dynamodb-access" \
+    --attach-policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess \
+    --approve --override-existing-serviceaccounts
+
+# Load the cluster with the Kubernetes deployment and service
 eksctl get clusters --region ap-southeast-2
 kubectl apply -f ../kubernetes/mainfest.yml
 kubectl get svc
