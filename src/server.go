67bc3705da7122110b303deaa8fb31d88d3225f3
Colin Schofield <colin.schofield@comparethemarket.com.au>
2022-09-17 13:24:26 +1000

    Gin CORS added

--- src/server.go
+++ src/server.go
@@ -23,9 +23,26 @@ const (
 	apiVersion = "/api/1.0"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	config.Logger().Info("Starting Rest API Service..")
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.GET(apiVersion+"/list/daily", covidController.GetCovid19DailySummary)
 	router.GET(apiVersion+"/list/regions", regionController.GetListOfRegions)
 	router.POST(apiVersion+"/user", userController.CreateUser)
